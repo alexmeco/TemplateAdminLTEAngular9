4e9155b87d6928975ee616211dab76efbd2d3b6d
Alex Meregildo <alexmeco95@gmail.com>
2020-02-23 23:35:17 -0500

    Plantilla Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { MenuComponent } from './components/shared/menu/menu.component';
+import { ContentComponent } from './components/shared/content/content.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { SettingComponent } from './components/shared/setting/setting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    ContentComponent,
+    FooterComponent,
+    SettingComponent
   ],
   imports: [
     BrowserModule
